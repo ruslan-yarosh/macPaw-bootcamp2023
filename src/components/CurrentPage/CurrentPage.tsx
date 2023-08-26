e83faac60ae15491eddf1d91232af5a5258cdd6a
Ruslan Yarosh <ruslan.yarosh.96@gmail.com>
2023-08-26 16:45:14 +0300

    feat: add ability to go back in 1 step in history, add uppercase to currentPage info

--- src/components/CurrentPage/CurrentPage.tsx
+++ src/components/CurrentPage/CurrentPage.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 
 import {ReactComponent as Back} from '../../assets/icons/back.svg';
 import './CurrentPage.scss';
+import { useNavigate } from 'react-router-dom';
 
 type Props = {
   pageName: string,
 }
 
 export const CurrentPage: React.FC<Props> = ({ pageName }) => {
+  const navigate = useNavigate();
+
   return (
     <div className="current-page">
-      <button type="button" className="current-page__back">
+      <button 
+        type="button" 
+        className="current-page__back"
+        onClick={() => navigate(-1)}
+      >
         <Back />
       </button>
 
