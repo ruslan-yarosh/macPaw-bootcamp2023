247c0b029cfce77847271f93eb6a5c1cb19b5f6a
Ruslan Yarosh <ruslan.yarosh.96@gmail.com>
2023-08-28 06:41:04 +0300

    feat: add routes to new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import { HomeSection } from './components/HomeSection';
@@ -6,8 +7,14 @@ import { StaticSection } from './components/StaticSection';
 import { VotingSection } from './components/VotingSection';
 import { FavouritesSection } from './components/FavouritesSection';
 import { VotingResult } from './components/VotingResult';
+import { BreedsSection } from './components/BreedsSection';
+import { BreedInfo } from './components/BreedInfo';
+import { SearchSection } from './components/SearchSection';
+import { GallerySection } from './components/GallerySection';
 
-function App() {
+export const App: React.FC = () => {
+  const [favouritesId, setFavouritesId] = useState<string[]>([]);
+
   return (
     <main className="page__main">
       <div className="container">
@@ -15,14 +22,31 @@ function App() {
 
         <Routes>
           <Route path="/" element={<HomeSection />} />
-          <Route path='/voting' element={<VotingSection />} />
-          <Route path='/favourites' element={<FavouritesSection />} />
+          <Route 
+            path='/voting' 
+            element={
+              <VotingSection />
+            } 
+          />
+          <Route path='/favourites' element={<FavouritesSection setFavouritesId={setFavouritesId} />} />
           <Route path='/likes' element={<VotingResult pageName='Likes' />} />
           <Route path='/dislikes' element={<VotingResult pageName='Dislikes' />} />
+
+          <Route path='breeds'>
+            <Route index element={<BreedsSection />} />
+            <Route path=":breedId" element={<BreedInfo />} />
+          </Route>
+
+          <Route 
+            path='/gallery' 
+            element={
+              <GallerySection favouritesId={favouritesId} setFavouritesId={setFavouritesId} />
+            } 
+          />
+
+          <Route path='/search' element={<SearchSection />} />
         </Routes>
       </div>
     </main>
   );
 }
-
-export default App
